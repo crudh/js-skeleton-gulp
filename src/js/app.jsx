815b82fb4743e9a8ca48ba57b2a3fcb57022785d
Christian Rudh <christian@rudh.se>
2014-02-06 19:38:52 +0100

    Fixed HTML and indentation.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,12 +3,12 @@
  */
 var React = require("react");
 
-    var AppContainer = React.createClass({
-        render: function() {
-            return <div>
-                <h1>REACT IS LIVE!</h1>
-            </div>;
-        }
-    });
+var AppContainer = React.createClass({
+    render: function() {
+        return <div>
+            <h1>REACT IS LIVE!</h1>
+        </div>;
+    }
+});
 
 module.exports = exports = AppContainer;
